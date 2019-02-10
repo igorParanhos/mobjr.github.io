a12d0814daa451b2b78e36165afec28e08ae88fd
Igor Paranhos <igor@2xt.com.br>
2019-02-10 16:31:05 -0200

    Add static files

---
+++ precache-manifest.1132f6a334af5270d0275cfcd6774216.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "93a1e6098211b5ad4681",
+    "url": "/static/js/main.93a1e609.chunk.js"
+  },
+  {
+    "revision": "fb06d94a6905c80451d0",
+    "url": "/static/js/1.fb06d94a.chunk.js"
+  },
+  {
+    "revision": "93a1e6098211b5ad4681",
+    "url": "/static/css/main.727679cf.chunk.css"
+  },
+  {
+    "revision": "ad9d52fc8256d65abe788cb88ccdf626",
+    "url": "/index.html"
+  }
+];
